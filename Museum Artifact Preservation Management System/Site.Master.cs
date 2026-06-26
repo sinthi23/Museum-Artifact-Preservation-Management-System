a5c0f185e81ceb07cbd4c004ebbdb251aa03c77d
sinthi23 <tanhaislamsinthi2003@gmail.com>
2026-06-26 13:46:34 +0600

    master page added

---
+++ Museum Artifact Preservation Management System/Site.Master.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Web.UI;
+
+namespace Museum_Artifact_Preservation_Management_System
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
